20cf6c315226e20444902f1f0b8f636a1fca5d55
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-30 16:21:03 -0500

    feat: drawer and BottomNavigationBar

--- lib/home.dart
+++ lib/home.dart
@@ -9,15 +9,33 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: new AppBar(
-         title: new Text("Sample title"),
-         actions: <Widget>[
-           new IconButton(
-             icon: new Icon(Icons.search),
-             onPressed: () {}
-           )
-         ],
-       ),
+      appBar: new AppBar(
+        title: new Text("Sample title"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.search), onPressed: () {})
+        ],
+      ),
+      drawer: new Drawer(),
+      bottomNavigationBar: new BottomNavigationBar(
+        items: _getFooterItems(),
+      ),
     );
+  }
+
+  List<BottomNavigationBarItem> _getFooterItems() {
+    return [
+      new BottomNavigationBarItem(
+        icon: new Icon(Icons.thumb_up),
+        title: new Text("Popular")
+      ),
+      new BottomNavigationBarItem(
+        icon: new Icon(Icons.update),
+        title: new Text("Coming Soon")
+      ),
+      new BottomNavigationBarItem(
+        icon: new Icon(Icons.star),
+        title: new Text("Highest Rated")
+      )
+    ];
   }
 }
