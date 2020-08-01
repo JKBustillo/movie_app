a15999a31b082d0acf81321017b5941de5b2f63f
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-31 20:55:49 -0500

    feat; return the response in a object list

--- lib/common/HttpHandler.dart
+++ lib/common/HttpHandler.dart
@@ -2,21 +2,24 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:movie_app/common/Secret.dart';
+import 'package:movie_app/model/Media.dart';
 
 class HttpHandler {
   final String _baseUrl = 'api.themoviedb.org';
 
   Future<dynamic> getJSON(Uri uri) async {
     http.Response response = await http.get(uri);
-    return json.decode(response.body).toString();
+    return json.decode(response.body);
   }
 
-  Future<String> fetchMovies() {
+  Future<List<Media>> fetchMovies() {
     var uri = new Uri.https(_baseUrl, '/3/movie/popular', {
       'api_key': API_KEY,
       'page': "1",
     });
 
-    return getJSON(uri).then((((data) => data.toString())));
+    return getJSON(uri).then(((data) => 
+      data["results"].map<Media>((item) => new Media(item)).toList()
+    ));
   }
 }
